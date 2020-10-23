5177719acd3e43951d6526c2017e303a266932e8
Myron <dubtwee@gmail.com>
2020-10-23 12:19:47 -0500

    getting all koalas with AJAX callto server GET

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,17 +1,16 @@
-console.log( 'js' );
+console.log('js');
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
+$(document).ready(function () {
+  console.log('JQ');
   // Establish Click Listeners
-  setupClickListeners()
+  setupClickListeners();
   // load existing koalas on page load
   getKoalas();
-
 }); // end doc ready
 
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
+  $('#addButton').on('click', function () {
+    console.log('in addButton on click');
     // get user input and put in an object
     // NOT WORKING YET :(
     // using a test object
@@ -23,18 +22,36 @@ function setupClickListeners() {
       notes: 'testName',
     };
     // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+    saveKoala(koalaToSend);
+  });
 }
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
+function getKoalas() {
+  console.log('in getKoalas');
   // ajax call to server to get koalas
-  
+  $.ajax({
+    method: 'GET',
+    url: '/koalas',
+  })
+    .then(function (response) {
+      // render to the DOM
+      render(response);
+    }) // successful response
+    .catch(function (err) {
+      console.log(err);
+      alert('Something went wrong retrieving koalas.');
+    }); // failure from server
 } // end getKoalas
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
+function saveKoala(newKoala) {
+  console.log('in saveKoala', newKoala);
   // ajax call to server to get koalas
- 
+}
+
+//
+// DOM INTERACTION
+// ------------------------------
+
+function render(listOfKoalas) {
+  console.log(listOfKoalas);
 }
