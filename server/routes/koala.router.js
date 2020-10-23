7f9de3b92ce0ab47e8a2f5f37a80f51cc55a7dc7
Myron <dubtwee@gmail.com>
2020-10-23 10:01:44 -0500

    stubbing in get and post routes for server

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,8 +5,16 @@ const pool = require('../modules/pool');
 // DB CONNECTION
 
 // GET
+// send back all koalas from DB
+koalaRouter.get('/', (req, res) => {
+  res.send('GET Hello');
+});
 
 // POST
+// Save new koala to DB
+koalaRouter.post('/', (req, res) => {
+  res.sendStatus(201);
+});
 
 // PUT
 
